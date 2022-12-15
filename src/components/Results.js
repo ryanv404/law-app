f794da5e26440ef948549bf94fc032376144ebed
Ryan <95035038+ryanv404@users.noreply.github.com>
2022-12-14 21:16:53 -0600

    init commit

---
+++ src/components/Results.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Results = () => {
+  return (
+    <div>Results will be here.</div>
+  )
+}
+
+export default Results
---
+++ src/components/Search.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const Search = () => {
+  const [text, setText] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Text submitted: ${text}`);
+    setText("");
+  };
+
+  return (
+    <div style={{"height": "500px"}}>
+      <h1>Law App</h1>
+      <p>Search for a case below.</p>
+      <form action="#" method="get" onSubmit={handleSubmit}>
+        <label htmlFor="case_search">
+          <input type="text" name="case_search" id="case_search"
+          onChange={(e) => setText(e.target.value)} value={text} autoComplete="off"
+          />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  )
+}
+
+export default Search
